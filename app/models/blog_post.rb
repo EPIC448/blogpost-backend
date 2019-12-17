34c7fa17bd9d7e67442dd26fbc86fdce10e293e5
epic0448 <samuelogundiran@Yahoo.com>
2019-12-17 14:52:18 -0500

    updated blogPost, Added a Scheme, and models

--- app/models/blog_post.rb
+++ app/models/blog_post.rb
@@ -3,36 +3,39 @@ class BlogPost < ApplicationRecord
     validates :title, presence: true
     validates :content, presence: true
 
+    #  Note. Update is not a Requirments
 
-    def update_blog(title, content)
-        # Update a blog, whenever we add to it.
-        if title == 'title' && content == 'content'
-           if  self.title != self.title 
-            self.title = "The new title entry"
+#     def update_blog(title, content)
+#         # Update a blog, whenever we add to it.
+#         if title == 'title' && content == 'content'
+#            if  self.title != self.title 
+#             self.title = "The new title entry"
 
-            #  Change what was entered ... You may not need to do a logic here. 
-#    thinking .  self.title = self.title.update
-#    BlogPost.update(id, :title = > 'new Title')
+#             #  Change what was entered ... You may not need to do a logic here. 
+# #    thinking .  self.title = self.title.update
+# #    BlogPost.update(id, :title = > 'new Title')
 
-            self.save
-           end
+#             self.save
+#            end
 
-        elsif
-            if self.content != self.content
-#   Change to the new content
+#         elsif
+#             if self.content != self.content
+# #   Change to the new content
 
-       self.content = "The new content entry"
-    #    thinking .  self.content = self.title.content 
+#        self.content = "The new content entry"
+#     #    thinking .  self.content = self.title.content 
 
-                self.save
-            end
+#                 self.save
+#             end
 
-        else
+#         else
 
-            return 'Enter the Title and Content'
+#             return 'Enter the Title and Content'
 
-        end
+#         end
 
-    end
+#     end
 
 end
+
+# */
